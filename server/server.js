3bdad25df94eb3e26981179bff42b8a5e5c88cd2
ARBUCHELI <radio_seattle@hotmail.com>
2025-02-24 18:13:25 +0300

    updating client and server

--- server/server.js
+++ server/server.js
@@ -3,20 +3,40 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const cors = require('cors');
-const backendUrl = "https://bookstore-ecommerce-mern-app.onrender.com";
+//const backendUrl = "https://bookstore-ecommerce-mern-app.onrender.com";
 
 const mongoURI = process.env.MONGO_URI || 'mongodb+srv://radioseattle:LgYndtSOqpSrfYdF@cluster0.95tuz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
-mongoose.connect(mongoURI, {
+/*mongoose.connect(mongoURI, {
 	useNewUrlParser: true, 
 	useUnifiedTopology: true, 
   })
 	.then(() => console.log('MongoDB connected'))
-	.catch((err) => console.log('Error connecting to MongoDB:', err));
+	.catch((err) => console.log('Error connecting to MongoDB:', err));*/
+mongoose.connect(mongoURI, { 
+		useNewUrlParser: true, 
+		useUnifiedTopology: true 
+})
+.then(() => {
+    console.log('MongoDB connected successfully');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
 
 app.use(express.json());
 //app.use(cors());
-const corsOptions = {
+/*const corsOptions = {
+	origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  };
+  app.use(cors(corsOptions));*/
+  const corsOptions = {
+	origin: '*',  // Allow all origins for testing, replace with a specific domain in production
+	methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  };
+  
+  app.use(cors(corsOptions));
+/*const corsOptions = {
 	origin: function (origin, callback) {
 	  if (!origin || origin === 'https://bookstore-ecommerce-mern-app.onrender.com' || origin === 'https://bookstore-ecommerce-mern-app-1.onrender.com') {
 		callback(null, true);  // Allow the request
@@ -26,7 +46,7 @@ const corsOptions = {
 	},
 	methods: ['GET', 'POST', 'PUT', 'DELETE'],
   };
-  app.use(cors(corsOptions));
+  app.use(cors(corsOptions));*/
   
 
 const bookSchema = new mongoose.Schema({
@@ -71,15 +91,17 @@ const seedDatabase = async () => {
 
 seedDatabase();
 
-app.get(`${backendUrl}/api/books`, async (req, res) => {
+app.get('/api/books', async (req, res) => {
 	try {
-		const allBooks = await Book.find();
-		res.json(allBooks);
+	  console.log("Fetching books...");
+	  const allBooks = await Book.find();
+	  console.log("Books fetched:", allBooks);
+	  res.json(allBooks);
 	} catch (error) {
-		console.error(error);
-		res.status(500).json({ error: 'Internal Server Error' });
+	  console.error("Error fetching books:", error);
+	  res.status(500).json({ error: 'Internal Server Error' });
 	}
-});
+  });
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
